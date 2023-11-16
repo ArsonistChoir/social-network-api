0f86e4450db25f97f9b841fe00fdcd787169e119
ArsonistChoir <haiden2004ellis@outlook.com>
2023-11-15 22:30:12 -0600

    Did some more work with routes

--- routes/api/index.js
+++ routes/api/index.js
@@ -1,6 +1,6 @@
 const router = require('express').Router();
-const thoughtsRoutes = require('./thoughtsRoutes')
-const usersRoutes = require('./thoughtsRoutes')
+const thoughtsRoutes = require('./thoughtsRoutes');
+const usersRoutes = require('./usersRoutes'); 
 
 router.use('/thoughts', thoughtsRoutes);
 router.use('/users', usersRoutes);
---
+++ routes/api/usersRoutes.js
@@ -0,0 +1,17 @@
+const router = require('express').Router();
+const {
+  getUsers,
+  getSingleUser,
+  createUser,
+  deleteUser
+} = require('../../controllers/usersController');
+
+router.route('/')
+  .get(getUsers)
+  .post(createUser);
+
+router.route('/:userId')
+  .get(getSingleUser)
+  .delete(deleteUser);
+
+module.exports = router;
